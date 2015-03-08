e9dfc82c925d739fbbf29ebc7d7010958ff25093
GunGeekATX <GunGeekATX@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-08 22:49:14 +0000

    v2.0.1 updates
    git-svn-id: https://plugins.svn.wordpress.org/dynamic-placeholder-images/trunk@1108433 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gga-dynamic-placeholder-images.php
+++ gga-dynamic-placeholder-images.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Dynamic Placeholder Images
-Plugin URI: https://github.com/petenelson/gga-dynamic-placeholder-images
+Plugin URI: https://wordpress.org/plugins/dynamic-placeholder-images/
 Description: Plugin for managing and serving up placeholder images (such as <a href="http://baconmockup.com/200/200" target="_blank">http://baconmockup.com/200/200</a>)
-Version: 2.0.0
+Version: 2.0.1
 Author: Pete Nelson (@GunGeekATX)
 Author URI: https://twitter.com/GunGeekATX
 Text Domain: gga-dynamic-placeholder-images
