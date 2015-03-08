f7335681ddcf9668ab48f89c8233ff9e8b16395e
GunGeekATX <GunGeekATX@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-08 22:29:53 +0000

    v2.0.0 release
    git-svn-id: https://plugins.svn.wordpress.org/dynamic-placeholder-images/trunk@1108422 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,13 @@
+<?php
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	wp_die( 'restricted access' );
+}
+
+// delete stats table
+global $wpdb;
+$table_name = $wpdb->prefix . 'gga_dynamic_images_stats';
+$sql = "DROP TABLE IF_EXISTS $table_name";
+$wpdb->query($sql);
+
+$sql = "DELETE FROM $wpdb->options WHERE option_name like '_gga-placeholder-image-for%'";
+$wpdb->query($sql);
